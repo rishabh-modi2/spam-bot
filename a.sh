9c51777a2ac1458d2f26f6b1501a73d5bdc94a64
rishabh-modi2 <88582334+rishabh-modi2@users.noreply.github.com>
2022-01-19 21:17:39 +0530

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/rishabh-modi2/spam-bot -b plugins
 cd spam-bot
-cp * ..
+cp -r * ..
 cd ..
 node --optimize_for_size --max_old_space_size=460 --gc_interval=100 main.js
 ./a.sh
